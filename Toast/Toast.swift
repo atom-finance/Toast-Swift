caf4d6371259e636ca13ab5938f0d829c835895a
Harrison <51843048+harriq@users.noreply.github.com>
2021-09-10 11:02:17 -0400

    Added custom case to ToastPosition enum

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -751,6 +751,7 @@ public enum ToastPosition {
     case top
     case center
     case bottom
+    case bottomAboveTabBar
     
     fileprivate func centerPoint(forToast toast: UIView, inSuperview superview: UIView) -> CGPoint {
         let topPadding: CGFloat = ToastManager.shared.style.verticalPadding + superview.csSafeAreaInsets.top
@@ -763,6 +764,8 @@ public enum ToastPosition {
             return CGPoint(x: superview.bounds.size.width / 2.0, y: superview.bounds.size.height / 2.0)
         case .bottom:
             return CGPoint(x: superview.bounds.size.width / 2.0, y: (superview.bounds.size.height - (toast.frame.size.height / 2.0)) - bottomPadding)
+        case .bottomAboveTabBar:
+            return CGPoint(x: superview.bounds.size.width / 2.0, y: (superview.bounds.size.height - (toast.frame.size.height / 2.0)) - (2.25 * bottomPadding))
         }
     }
 }
